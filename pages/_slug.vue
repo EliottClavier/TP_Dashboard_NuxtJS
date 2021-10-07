85ec31e9b378cdef0e61d0537d4bd2fa2134db8e
Julien <julien4915@hotmail.com>
2021-10-07 16:59:29 +0100

    Dashboard et bouton suppr déconnexion

---
+++ pages/_slug.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>
+            <h1>Infos utilisateur</h1>
+        </div>
+        <v-row>  
+            <v-col cols="6">
+            <v-card elevation="2">
+            <v-card-text >Nom</v-card-text>
+            <v-divider></v-divider>
+            <v-card-text >Adresse email</v-card-text>
+            <v-divider></v-divider>
+            <v-card-text >Mot de passe</v-card-text>
+            </v-card>
+            </v-col>
+            <v-col cols="6">
+                <v-btn depressed color="error">
+                    Se déconnecter
+                </v-btn>
+                <v-row>
+                <v-btn class="ma-2" outlined color="error">
+                    Supprimer son compte
+                </v-btn>
+                </v-row>
+            </v-col>
+        </v-row>
+    </div>
+    
+</template>
